c3db669716afc48f7a88910bc283341ba8f47e59
Swayam Gupta <swayamgupta999@gmail.com>
2025-07-13 16:43:58 +0530

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter, Poppins } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 import { GlobalAnimatedBackground } from '@/components/global-animated-background'
 import './globals.css'
 
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body className={`${inter.variable} ${poppins.variable} font-sans antialiased`}>
         <GlobalAnimatedBackground />
         {children}
+        <Analytics />
       </body>
     </html>
   )
